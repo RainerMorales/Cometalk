e54fb9e373464ca5d5f9e98a66657da2d519d603
RainerMorales <rainermorales213@gmail.com>
2025-06-18 22:19:29 +0800

    setup firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,28 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
+      <div className=" flex items-center justify-center text-center p-4 text-white bg-zinc-800  font-bold text-2xl  border-b">
+        Cometalk
+      </div>
       <main className="h-screen max-w-2xl m-auto   text-white">
-        <div className=" flex items-center justify-center text-center p-4 text-black  font-bold text-2xl   border-b">
-          Cometalk
-        </div>
         <div className="h-[70%] m-4 rounded">
-          <div className="min-h-30 border bg-zinc-700  p-4 m-2 rounded-xl space-y-4">
+          <div className="min-h-30 border bg-zinc-800  p-4 m-2 rounded-xl space-y-4">
             <div className=" text-sm flex justify-between opacity-60">
               <div>Rainer</div>
               <div>May10</div>
             </div>
             <div>
-              <div>
-               Hi Rainer morales!
-              </div>
+              <div>Hi Rainer morales!</div>
             </div>
           </div>
         </div>
         <div className="h-20 flex items-center justify-center border-t rounded-2xl">
           <div className="flex w-full max-w-sm items-center space-x-2 m-4">
-            <Input className="rounded-full h-10 text-black" type="text" placeholder="Message here" />
+            <Input
+              className="rounded-full h-10 text-black"
+              type="text"
+              placeholder="Message here"
+            />
             <Button className="bg-black text-white" type="submit">
               Send
             </Button>
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,19 +10,21 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+import { auth } from "../../firebase";
+import { useState } from "react";
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="bg-black text-white border-zinc-800">
+      <Card className=" text-black ">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+          <CardDescription>Enter your Username below</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
@@ -36,28 +39,28 @@ export function LoginForm({
                   required
                 />
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+              <div className="grid gap-2 ">
+                <Label htmlFor="email">Password</Label>
                 <Input
                   className="border-zinc-800"
                   id="password"
                   type="password"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2 ">
+                <Label htmlFor="email">Username</Label>
+                <Input
+                  className="border-zinc-800"
+                  id="Name"
+                  type="Name"
+                  placeholder="Juan"
                   required
                 />
               </div>
               <Button type="submit" className="w-full">
                 Login
-              </Button>
-              <Button variant="outline" className="w-full text-black">
-                Login with Google
               </Button>
             </div>
             <div className="mt-4 text-center text-sm ">
