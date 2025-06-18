2502aa0f42f796f7f26ef1f45dd3019fc6584ab0
RainerMorales <rainermorales213@gmail.com>
2025-06-18 21:41:58 +0800

    change ui color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <main className="h-screen max-w-2xl m-auto  border-2 border-zinc-900  text-white">
-        <div className=" flex items-center justify-center text-center p-4  font-bold text-2xl">
+      <main className="h-screen max-w-2xl m-auto   text-white">
+        <div className=" flex items-center justify-center text-center p-4 text-black  font-bold text-2xl   border-b">
           Cometalk
         </div>
-        <div className="h-[70%] m-4 rounded bg-zinc-950">
-          <div className="min-h-30 bg-zinc-900 p-4 m-2 rounded-xl space-y-4">
+        <div className="h-[70%] m-4 rounded">
+          <div className="min-h-30 border bg-zinc-700  p-4 m-2 rounded-xl space-y-4">
             <div className=" text-sm flex justify-between opacity-60">
               <div>Rainer</div>
               <div>May10</div>
@@ -21,10 +21,10 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="h-20 flex items-center justify-center border border-zinc-900">
+        <div className="h-20 flex items-center justify-center border-t rounded-2xl">
           <div className="flex w-full max-w-sm items-center space-x-2 m-4">
-            <Input type="text" placeholder="Message here" />
-            <Button className="bg-white text-black" type="submit">
+            <Input className="rounded-full h-10 text-black" type="text" placeholder="Message here" />
+            <Button className="bg-black text-white" type="submit">
               Send
             </Button>
           </div>
